23e6b44b0d16c2af3e12ffe7bac66021416720b4
Ademayowa <ademayowaadeniyi@gmail.com>
2021-08-09 13:36:06 +0100

    completed info section

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Header from '@/components/Header';
 import Search from '@/components/search/Search';
 import Hero from 'sections/Hero';
+import Info from 'sections/Info';
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-      {/* Hero */}
+      <Info />
       {/* Search */}
     </>
   );
