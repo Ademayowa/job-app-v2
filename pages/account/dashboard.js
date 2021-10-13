b3eb17ec424670cd5d85e682c843635d6f0cf2b7
Ademayowa <ademayowaadeniyi@gmail.com>
2021-10-13 15:33:17 +0100

    gets the token from backend into the dashboard

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,32 @@
+import { parseCookie } from '@/helpers/index';
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function DashboardPage() {
+export default function DashboardPage({ jobs }) {
+  console.log(jobs);
+
   return (
     <Layout title='User Dashboard'>
       <h1>Dashboard</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookie(req);
+
+  const res = await fetch(`${API_URL}/jobs/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const jobs = await res.json();
+
+  return {
+    props: {
+      jobs,
+    },
+  };
 }
